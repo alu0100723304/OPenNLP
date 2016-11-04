3f57afa7db70c59ae055b4a4e85fb5c2bb548bda
Joey <Joey@192.168.1.41>
2016-11-04 20:52:15 +0000

    Leer ficheros de un directorio

--- src/main/java/aplicacion/LeerDirectorio.java
+++ src/main/java/aplicacion/LeerDirectorio.java
@@ -1,15 +1,17 @@
 package aplicacion;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class LeerDirectorio {	
+public class LeerDirectorio {
 	private File folder_;
 
 	public LeerDirectorio(File folder_) {
 		super();
 		this.folder_ = folder_;
 	}
-		
+
 	public File getFolder_() {
 		return folder_;
 	}
@@ -20,11 +22,15 @@ public class LeerDirectorio {
 
 	public void listFilesForFolder(final File folder) {
 		for (final File fileEntry : folder.listFiles()) {
-			if (fileEntry.isDirectory()) {
-				listFilesForFolder(fileEntry);
+			System.out.println(fileEntry.getName());
+			try {
+				Scanner s = new Scanner(fileEntry);
+				System.out.println(s.nextLine());
+				s.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			else
-				System.out.println(fileEntry.getName());
 		}
 	}
 }
